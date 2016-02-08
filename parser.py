b41ce2be52bc3aebf5ad9a737700da0753602670
Kashav Madan <kshvmdn@gmail.com>
2016-02-08 18:06:11 -0500

    Use OrderedDict for repo data

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import requests
+import collections
 
 API_URL = 'https://api.github.com/users/{}/repos'
 
@@ -18,15 +19,15 @@ def parse(response):
         return None
     for repo in repos:
         if 'name' in repo and not repo['fork']:
-            data.append({
-                'name': repo['name'],
-                'desc': repo['description'],
-                'lang': repo['language'],
-                'stars': repo['stargazers_count']
-            })
+            data.append(collections.OrderedDict([('name', repo['name']),
+                                                 ('desc', repo['description']),
+                                                 ('lang', repo['language']),
+                                                 ('stars', repo['stargazers_count'])]))
     return data
+
 
 if __name__ == '__main__':
     import pprint
+
     u = 'kshvmdn'
     pprint.pprint(main(u))

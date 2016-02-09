5d8a37cdbd41af594f03d78092b78a22afc53c05
Kashav Madan <kshvmdn@gmail.com>
2016-02-09 13:00:41 -0500

    Add support for multiple users, format types

--- __main__.py
+++ __main__.py
@@ -6,16 +6,29 @@ from githublist.parser import main as get_data
 from githublist.serve import serve_content
 
 parser = argparse.ArgumentParser(description='View repositories for any GitHub account.')
-parser.add_argument('user', type=str, help='GitHub user handle')
-parser.add_argument('-f', '--format', choices=['json', 'csv', 'md', 'raw.txt', 'tbl.txt'],
+parser.add_argument('user', type=str, nargs='+', help='GitHub user handle')
+parser.add_argument('-f', '--format', nargs='+',
+                    choices=['json', 'csv', 'md', 'raw.txt', 'tbl.txt', 'all'],
                     help='File output format.')
 
 
 def main():
     args = parser.parse_args()
-    user, format_ = args.user, args.format
+    user = args.user
+    format_ = ['json', 'csv', 'md', 'raw.txt', 'tbl.txt'] if 'all' in args.format else args.format
 
-    return serve_content(get_data(user), user, format_)
+    for u in user:
+        print('Preparing data for {}...'.format(u))
+        d = get_data(u)
+        for f in format_:
+            if f is not None:
+                print(' Writing {}...'.format(f), end='')
+                serve_content(d, u, f)
+                print(' Done!')
+            else:
+                serve_content(d, u, f)
+    print('Complete!')
+    return None
 
 if __name__ == '__main__':
     main()

ffe68f52098754109299fed176caba1d4010bdc9
Kashav Madan <kshvmdn@gmail.com>
2016-02-09 12:12:28 -0500

    Move __main__

--- __main__.py
+++ __main__.py
@@ -2,20 +2,20 @@
 
 import argparse
 
-from parser import main as get_data
-from write import write_data
+from githublist.parser import main as get_data
+from githublist.serve import serve_content
 
 parser = argparse.ArgumentParser(description='View repositories for any GitHub account.')
 parser.add_argument('user', type=str, help='GitHub user handle')
 parser.add_argument('-f', '--format', choices=['json', 'csv', 'md', 'txt'],
                     help='File output format.')
 
-args = parser.parse_args()
-user, format_ = args.user, args.format
 
-write_data(get_data(user), user, format_)
+def main():
+    args = parser.parse_args()
+    user, format_ = args.user, args.format
+
+    return serve_content(get_data(user), user, format_)
 
 if __name__ == '__main__':
-    user = 'kshvmdn'
-    data = get_data(user)
-    write_data(data, user, 'json')
+    main()

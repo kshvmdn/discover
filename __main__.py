751692ddb44171d65c30f3277373566f38a2d084
Kashav Madan <kshvmdn@gmail.com>
2016-02-09 13:11:57 -0500

    Add default format type

--- __main__.py
+++ __main__.py
@@ -8,26 +8,25 @@ from githublist.serve import serve_content
 
 parser = argparse.ArgumentParser(description='View repositories for any GitHub account.')
 parser.add_argument('user', type=str, nargs='+', help='GitHub user handle')
-parser.add_argument('-f', '--format', nargs='+',
+parser.add_argument('--format', nargs='+',
                     choices=['json', 'csv', 'md', 'raw.txt', 'tbl.txt', 'all'],
-                    help='File output format.')
+                    help='File output format. default=tbl.txt', default=['tbl.txt'])
 
 
 def main():
     args = parser.parse_args()
     user = args.user
-    format_ = ['json', 'csv', 'md', 'raw.txt', 'tbl.txt'] if 'all' in args.format else args.format
+    format_ = args.format
+    if 'all' in args.format:
+        format_ = ['json', 'csv', 'md', 'raw.txt', 'tbl.txt']
 
     for u in user:
         print('Preparing data for {}...'.format(colored(u, 'white')))
         d = get_data(u)
         for f in format_:
-            if f is not None:
-                print(' Writing {}...'.format(colored(f, 'white')), end='')
-                serve_content(d, u, f)
-                print(' Done!')
-            else:
-                serve_content(d, u, f)
+            print(' Writing {}...'.format(colored(f, 'white')), end='')
+            serve_content(d, u, f)
+            print(' Done!')
     cprint('Complete!', 'red')
     return None
 

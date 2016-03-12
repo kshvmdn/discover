88aa4f6eefeb6fa6524a4a38dc3c9be03badc0c6
Kashav Madan <kshvmdn@gmail.com>
2016-03-12 18:56:07 -0500

    PEP8

--- main.py
+++ main.py
@@ -6,11 +6,17 @@ from termcolor import colored, cprint
 from githublist.parser import main as get_data
 from githublist.serve import serve_content
 
-parser = argparse.ArgumentParser(description='View repositories for any GitHub account.')
-parser.add_argument('user', type=str, nargs='+', help='GitHub user handle')
-parser.add_argument('-f', '--format', nargs='+',
+parser = argparse.ArgumentParser(
+                    description='View repositories for any GitHub account.')
+parser.add_argument('user',
+                    type=str,
+                    nargs='+',
+                    help='GitHub user handle')
+parser.add_argument('-f', '--format',
+                    nargs='+',
                     choices=['json', 'csv', 'md', 'raw.txt', 'tbl.txt', 'all'],
-                    help='File output format, default=tbl.txt', default=['tbl.txt'])
+                    help='File output format, default=tbl.txt',
+                    default=['tbl.txt'])
 
 
 def main():
